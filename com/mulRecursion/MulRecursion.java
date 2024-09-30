9d50f642d6c057fbda3edab586a5fbd86bf43f96
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2024-09-29 21:17:20 -0600

    Update MulRecursion.java
    create recursion for multiplication

--- com/mulRecursion/MulRecursion.java
+++ com/mulRecursion/MulRecursion.java
@@ -1,6 +1,15 @@
 package com.mulrecursion;
 
 public class MulRecursion {
+    public static int mul(int a, int b) {
+        if (b == 0) {
+            return 0;
+        } else if (b > 0) {
+            return a + mul(a, b - 1);
+        } else { // b < 0 (handle negatives)
+            return -mul(a, -b);
+        }
+    }
     public static void main(String[] args) {
         
     }
